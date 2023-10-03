34dfcce1be4d5033968107466d9f7e56f107398b
David Aslan <davidaslan112671@live.se>
2023-10-04 01:34:34 +0200

    done with step 9 starting step 10

--- Sirocco test2/Program.cs
+++ Sirocco test2/Program.cs
@@ -37,30 +37,57 @@ namespace Sirocco_test2
             }
 
 
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadLine();
+
 
             void CrmActionsPicker(CrmServiceClient crmServiceClient)
             {
-                Console.WriteLine("Choose an action\n1.To create an account\n2.To create a contact\n3.To QueryTheDb");
-                string action = Console.ReadLine();
+                bool runLoop = true;
 
-                switch (action)
+
+                while (runLoop)
                 {
+                    Console.WriteLine("Choose an action" +
+                  "\n1.To create an account" +
+                  "\n2.To create a contact" +
+                  "\n3.To QueryTheDb" +
+                  "\n4.To Update a account" +
+                  "\n5.To Update a contact" +
+                  "\n6.To Create a note" +
 
-                    case "1":
-                        CreateAccount(crmServiceClient);
-                        break;
-                    case "2":
-                        CreateContact(crmServiceClient);
-                        break;
-                    case "3":
-                        QueryTheDb(crmServiceClient);
-                        break;
+                  "\n\nPress any other key to exit");
+                    string action = Console.ReadLine();
 
-                    default:
-                        break;
+                    switch (action)
+                    {
+
+                        case "1":
+                            CreateAccount(crmServiceClient);
+                            break;
+
+                        case "2":
+                            CreateContact(crmServiceClient);
+                            break;
+
+                        case "3":
+                            QueryTheDb(crmServiceClient);
+                            break;
+                        case "4":
+                            UpdateAccount(crmServiceClient);
+                            break;
+                        case "5":
+                            UpdateContact(crmServiceClient);
+                            break;
+                        case "6":
+                            CreateNotes(crmServiceClient);
+                            break;
+
+                        default:
+                            runLoop = false;
+
+                            break;
+                    }
                 }
+
             }
             void QueryTheDb(CrmServiceClient crmServiceClient)
             {
@@ -69,12 +96,28 @@ namespace Sirocco_test2
 
                 EntityCollection result = crmServiceClient.RetrieveMultiple(query);
             }
+
+            bool YesOrNo(string yesOrNoQuestion)
+            {
+                Console.WriteLine(yesOrNoQuestion);
+                string yesOrNo = Console.ReadLine();
+                if (yesOrNo == "y")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
             void CreateAccount(CrmServiceClient crmServiceClient)
             {
-                Console.WriteLine("Creating account...");
+                Console.WriteLine("What do you wish to name the new account?");
+                string accountName = Console.ReadLine();
+                bool toAddParent = YesOrNo("Do you wish to assign a parent account? y/n");
 
                 Entity account = new Entity("account");
-                account["name"] = "Test Account";
+                account["name"] = accountName;
                 account["telephone1"] = "555-555-5555";
                 account["address1_line1"] = "123 Main St.";
                 account["address1_city"] = "Seattle";
@@ -82,27 +125,98 @@ namespace Sirocco_test2
                 account["address1_postalcode"] = "98101";
                 account["address1_country"] = "US";
 
+                if (toAddParent)
+                {
+                    Console.WriteLine("insert the parent id?");
+                    var parentId = Console.ReadLine();
+                    Console.WriteLine("assigning parent to account...");
+                    account["parentaccountid"] = new EntityReference("account", new Guid(parentId));
+                }
+
                 var result = crmServiceClient.Create(account);
                 Console.WriteLine($"created {result}");
+
             }
             void CreateContact(CrmServiceClient crmServiceClient)
             {
+                Console.WriteLine("What is the FirstName of the new Contact?");
+                string fName = Console.ReadLine();
+                Console.WriteLine("What is the LastName of the new Contact?");
+                string lName = Console.ReadLine();
+
+
                 Console.WriteLine("Creating contact...");
+                Entity contact = new Entity("contact");
+                contact["firstname"] = fName;
+                contact["lastname"] = lName;
+                contact["emailaddress1"] = "createdFrom@consoleApp.com";
+                contact["telephone1"] = "555-555-5556";
 
-                Entity account = new Entity("account");
-                account["name"] = "Test Account";
-                account["telephone1"] = "555-555-5555";
-                account["address1_line1"] = "123 Main St.";
-                account["address1_city"] = "Seattle";
-                account["address1_stateorprovince"] = "WA";
-                account["address1_postalcode"] = "98101";
-                account["address1_country"] = "US";
+                bool toAssignToAccount = YesOrNo("Do you wish to assign the contact to an account? y/n");
+                if (toAssignToAccount)
+                {
+                    Console.WriteLine($"provice the accountId you wish to assign {fName} {lName} to");
+                    string accountId = Console.ReadLine();
+                    // Create a EntityReference to the account and set it as the parent customer.
+                    EntityReference parentAccount = new EntityReference("account", Guid.Parse(accountId));
+                    contact["parentcustomerid"] = parentAccount;
+                }
 
-                var result = crmServiceClient.Create(account);
-                Console.WriteLine($"created {result}");
+
+                var contactId = crmServiceClient.Create(contact);
+                Console.WriteLine($"Contact created with ID: {contactId}");
             }
+            void UpdateContact(CrmServiceClient crmServiceClient)
+            {
+                Console.WriteLine("What is the id of the contact you wish to update?");
+                var contactId = Console.ReadLine();
+
+                Console.WriteLine("what do you want to change the firstname to?");
+                string fName = Console.ReadLine();
+                Console.WriteLine("what do you want to change the lastname to?");
+                string lName = Console.ReadLine();
+                Console.WriteLine("what do you want to change the email to?");
+                string email = Console.ReadLine();
+                Console.WriteLine("what do you want to change the phone number to?");
+                string phone = Console.ReadLine();
+
+                Entity contactToUpdate = new Entity("contact");
+                contactToUpdate["contactid"] = Guid.Parse(contactId);
+                contactToUpdate["firstname"] = fName;
+                contactToUpdate["lastname"] = lName;
+                contactToUpdate["emailaddress1"] = email;
+                contactToUpdate["telephone1"] = phone;
+
+                crmServiceClient.Update(contactToUpdate);
+                Console.WriteLine("Contact updated successfully.");
+
+            }
+            void UpdateAccount(CrmServiceClient crmServiceClient)
+            {
+                Console.WriteLine("What is the id of the account you wish to update?");
+                var accountId = Console.ReadLine();
+                Console.WriteLine("what name do you want?");
+                var accountName = Console.ReadLine();
+                Console.WriteLine("what phone number do you want?");
+                var accountPhone = Console.ReadLine();
+                Console.WriteLine("what address do you want?");
+                var accountAddress = Console.ReadLine();
+
+                Entity accountToUpdate = new Entity("account");
+                accountToUpdate["accountid"] = Guid.Parse(accountId);
+                accountToUpdate["name"] = accountName;
+                accountToUpdate["telephone1"] = accountPhone;
+                accountToUpdate["address1_line1"] = accountAddress;
+                crmServiceClient.Update(accountToUpdate);
+
+                Console.WriteLine("Account updated successfully.");
+            }
+            void CreateNotes(CrmServiceClient crmServiceClient) { }
         }
 
 
     }
+
+
 }
+
